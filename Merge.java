e534aa48c30a06abe72acf2398b5068cdb384904
Michael Zhang <mzhang00@stuy.edu>
2019-03-15 09:52:17 -0400

    Add helper method for mergesort

--- Merge.java
+++ Merge.java
@@ -1,5 +1,9 @@
 public class Merge{
   public static void mergesort(int[]data){
+    int[] temp = new int[data.length];
+    mergesortH(data, temp, 0, data.length - 1);
+  }
+  private static void mergesortH(int[] data, int[] copy, int start, int end){
     //
   }
 }
